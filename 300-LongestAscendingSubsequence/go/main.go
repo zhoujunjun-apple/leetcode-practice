c57872820c247612337eb40ec227d1dafe1395ba
zhoujj <nujnujouhz@foxmail.com>
2020-11-18 13:45:24 +0800

    [add] add native dp for leetcode-300. same time and space complexity

--- 300-LongestAscendingSubsequence/go/main.go
+++ 300-LongestAscendingSubsequence/go/main.go
@@ -31,9 +31,32 @@ func BruteForce(s string) int {
 	return ret
 }
 
+// NativeDP function implement native dp solution
+// it has the same time and space cost with brute force solution
 func NativeDP(s string) int {
 	dp := make([]int, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = 1
+	}
 
+	ret := 0
+	for pos := 0; pos < len(s); pos++ {
+		posNum, _ := strconv.Atoi(s[pos : pos+1])
+		longest := 0
+		for back := pos - 1; back >= 0; back-- {
+			backNum, _ := strconv.Atoi(s[back : back+1])
+			if backNum < posNum && dp[back] > longest {
+				longest = dp[back]
+			}
+		}
+		dp[pos] += longest
+
+		if dp[pos] > ret {
+			ret = dp[pos]
+		}
+	}
+
+	return ret
 }
 
 func main() {
--- 300-LongestAscendingSubsequence/go/main_test.go
+++ 300-LongestAscendingSubsequence/go/main_test.go
@@ -2,29 +2,52 @@ package main
 
 import "testing"
 
-func TestBruteForce(t *testing.T) {
-	ts := []struct {
-		input string
-		exput int
-	}{
-		{
-			input: "43210",
-			exput: 1,
-		},
-		{
-			input: "21340532",
-			exput: 4,
-		},
-		{
-			input: "012345000",
-			exput: 6,
-		},
-	}
+var ts = []struct {
+	input string
+	exput int
+}{
+	{
+		input: "43210",
+		exput: 1,
+	},
+	{
+		input: "21340532",
+		exput: 4,
+	},
+	{
+		input: "012345000",
+		exput: 6,
+	},
+}
 
+func TestBruteForce(t *testing.T) {
 	for i, tc := range ts {
 		got := BruteForce(tc.input)
 		if got != tc.exput {
 			t.Errorf("%d-th input=%s, expected=%d, got=%d", i, tc.input, tc.exput, got)
 		}
+	}
+}
+
+func BenchmarkBruteForce(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := "21340532"
+		BruteForce(s)
+	}
+}
+
+func TestNativeDP(t *testing.T) {
+	for i, tc := range ts {
+		got := NativeDP(tc.input)
+		if got != tc.exput {
+			t.Errorf("%d-th input=%s, expected=%d, got=%d", i, tc.input, tc.exput, got)
+		}
+	}
+}
+
+func BenchmarkNativeDP(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := "21340532"
+		NativeDP(s)
 	}
 }
